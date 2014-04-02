4e03b126179cb1d17c19311a04151025c0be549d
James Kermode <james.kermode@gmail.com>
2014-04-02 10:50:46 +0100

    Added test of get/set routines and array access

--- examples/mockderivetype/test.py
+++ examples/mockderivetype/test.py
@@ -11,6 +11,19 @@ l2_handle = pysrc.f90wrap_leveltwo_initialise(3.0)
 # call a function inside leveltwomod
 pysrc.f90wrap_leveltwo_print(l2_handle)
 
+print pysrc.f90wrap_leveltwo__get__rl(l2_handle)
+pysrc.f90wrap_leveltwo__set__rl(l2_handle, 2.)
+pysrc.f90wrap_leveltwo__get__rl(l2_handle)
+
+# get an array pointer
+import f90wrap.arraydata
+from f90wrap.sizeof_fortran_t import sizeof_fortran_t
+vec = f90wrap.arraydata.get_array(sizeof_fortran_t(),
+                                  a_handle,
+                                  pysrc.f90wrap_atype__array__vec)
+vec[:] = 0.
+print vec
+
 # clear up and deallocate
 pysrc.f90wrap_atype_finalise(a_handle)
 pysrc.f90wrap_leveltwo_finalise(l2_handle)
