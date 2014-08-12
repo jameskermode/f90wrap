48b1c51c43a4f53ad5ddd4b82f5dd8f25b8776cb
James Kermode <james.kermode@gmail.com>
2014-08-12 15:17:59 +0100

    Use original file names to make Makefile construction easier

--- f90wrap/f90wrapgen.py
+++ f90wrap/f90wrapgen.py
@@ -107,7 +107,8 @@ class F90WrapperGenerator(ft.FortranVisitor, cg.CodeGenerator):
                     self._write_sc_array_wrapper(node, el, dims, self.sizeof_fortran_t)
 
         if len(self.code) > 0:
-            f90_wrapper_file = open('%s%s.f90' % (self.prefix, node.name), 'w')
+            f90_wrapper_file = open('%s%s.f90' % (self.prefix,
+                                                  os.path.basename(node.filename)), 'w')
             f90_wrapper_file.write(str(self))
             f90_wrapper_file.close()
         self.code = []
