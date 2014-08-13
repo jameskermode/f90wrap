40fce2f60ed0ef2e5b18390092f55f8c3e37e8d1
James Kermode <james.kermode@gmail.com>
2014-08-13 14:07:57 +0100

    Removed env.sh as does not work with compiled modules

--- env.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-
-ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-PLATFORM=`python -c "from distutils.util import get_platform ; from distutils.sysconfig import get_python_version ; print '%s-%s' % ( get_platform(), get_python_version() )"`
-
-export PYTHONPATH="$ROOT/build/lib.$PLATFORM:$PYTHONPATH"
