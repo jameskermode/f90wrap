9a5cc65b9caafd5c929571f79808a381c7933add
James Kermode <james.kermode@gmail.com>
2014-08-14 15:21:31 +0100

    Added test script for package mode

---
+++ examples/mockderivetype/testpkg.py
@@ -0,0 +1,58 @@
+import numpy as np
+
+from mockdtpkg import (define_a_type,
+                    leveltwomod,
+                    use_a_type,
+                    top_level)
+
+a = define_a_type.Atype() # calls initialise()
+
+a.rl = 3.0 # calls set()
+assert(a.rl == 3.0)
+
+a.vec[:] = 0. # calls get() then sets array data in place
+assert(np.all(a.vec == 0.0))
+a.vec = 1.
+assert(np.all(a.vec == 1.0))
+
+a.dtype.rl = 1.0 # calls set()
+
+my_l2 = leveltwomod.Leveltwo(4.0) # calls initialise()
+a.dtype = my_l2 # calls set()
+assert(a.dtype.rl == my_l2.rl)
+
+# access the module-level variables in use_a_type
+use_a_type.get_p()
+use_a_type.p.rl = 1.0
+use_a_type.p.bool = True
+use_a_type.p.integ = 10
+
+# call a routine in use_a_type
+result = use_a_type.do_stuff(8)
+assert(result == 1073741824.0)
+
+use_a_type.get_array_vector()
+use_a_type.vector[:] = 1.0
+assert(np.all(use_a_type.vector == 1.0))
+
+input = 3.0
+output = top_level(input)
+assert(output == 85.0*input)
+
+# test access to array of derived types p_array
+assert(len(use_a_type.p_array) == 3)
+for i in range(len(use_a_type.p_array)):
+    assert(use_a_type.p_array[i].bool == use_a_type.p.bool)
+    assert(use_a_type.p_array[i].integ == use_a_type.p.integ)
+    assert(use_a_type.p_array[i].rl == use_a_type.p.rl)
+    assert(np.all(use_a_type.p_array[i].vec == use_a_type.p.vec))
+
+# test function with derived type return type
+a = define_a_type.return_a_type_func()
+assert(a.bool == 1)
+assert(a.integ == 42)
+    
+# test subroutine with intent(out) derived type argument
+a = define_a_type.return_a_type_sub()
+assert(a.bool == 1)
+assert(a.integ == 42)
