0004184c067fcb22cac7056ac13181dc91ff8db7
rpfische <robert.fischer@nasa.gov>
2015-05-12 18:58:05 -0400

    Original source for example.

---
+++ examples/passbyreference/example_mymodule.py
@@ -0,0 +1,14 @@
+import mymodule
+import numpy as np
+
+tt = mymodule.Mymodule.Mytype()
+tt.val = 17
+b=np.array(17)
+
+print '********* BEFORE'
+print 'b=',b
+print 'tt=',tt
+mymodule.mymodule.mysubroutine(4,b,tt)
+print '********* AFTER'
+print 'b=',b
+print 'tt=',tt
