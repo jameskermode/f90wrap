98b3e3a342858f485196516402a7e4cbd7cb9daa
James Kermode <james.kermode@gmail.com>
2016-07-26 11:31:53 +0100

    Fix for issue #50 - use float32 rather than just float for single precion arrays

--- examples/arrays_in_derived_types_issue50/tests.py
+++ examples/arrays_in_derived_types_issue50/tests.py
@@ -1,8 +1,10 @@
 from issue50 import module_test as tp
-from numpy import zeros, ones, float32
+from numpy import zeros, ones, float32, abs, max
 
 a = tp.Real_Array()
 a.item = ones(6, dtype=float32)  # ones leads to a segfault 11 upon calling tp.testf(a); but zeros does not ... ?
 print("This is sent to fortran : " + str(a.item))
 tp.testf(a)
 print("This is received by python : " + str(a.item))
+
+assert abs(max(a.item - [1.0, 1.0, 1.0, 4.0, 1.0, 1.0])) < 1e-6
