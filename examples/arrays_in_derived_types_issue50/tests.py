0c74ea6235a16a4bbcd70bda417655dbbf65446e
MrYann <yannibu@gmail.com>
2016-07-28 13:13:26 +0200

    small bug fix

--- examples/arrays_in_derived_types_issue50/tests.py
+++ examples/arrays_in_derived_types_issue50/tests.py
@@ -2,9 +2,10 @@ from issue50 import module_test as tp
 from numpy import zeros, ones, float32, abs, max
 
 a = tp.Real_Array()
-a.item = ones(6, dtype=float32)  # ones leads to a segfault 11 upon calling tp.testf(a); but zeros does not ... ?
+print("This is the freshly allocated array : " + str(a.item))
+a.item = ones(6, dtype=float32) 
 print("This is sent to fortran : " + str(a.item))
 tp.testf(a)
 print("This is received by python : " + str(a.item))
 
-assert abs(max(a.item - [1.0, 1.0, 1.0, 4.0, 1.0, 1.0])) < 1e-6
+assert max(abs(a.item - [1.0, 1.0, 1.0, 4.0, 1.0, 1.0])) < 1e-6
