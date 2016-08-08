d44e4d8c3d52c22311c6ac084d779058f2e39cca
James Kermode <james.kermode@gmail.com>
2016-08-08 17:23:32 +0100

    remove legacy dependency code

--- setup.py
+++ setup.py
@@ -26,14 +26,6 @@ if (major, minor) < (2, 4):
     sys.exit(1)
 
 try:
-    import numpy
-    if not tuple([int(x) for x in numpy.__version__.split('.')[0:2]]) >= (1, 3):
-        raise ImportError
-except ImportError:
-    sys.stderr.write('Numpy 1.3 (http://www.numpy.org) or later needed to use this package\n')
-    sys.exit(1)
-
-try:
     import setuptools
 except ImportError:
     pass
