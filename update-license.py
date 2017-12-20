684a752d7a0b797f0282ba837a07b2fbd1e87a7f
James Kermode <james.kermode@gmail.com>
2017-12-20 14:51:54 +0000

    Fix license headers to correctly reflect use of LGPL v3

---
+++ update-license.py
@@ -0,0 +1,89 @@
+#  f90wrap: F90 to Python interface generator with derived type support
+#
+#  Copyright James Kermode 2011-2018
+#
+#  This file is part of f90wrap
+   For the latest version see github.com/jameskermode/f90wrap
+#
+#  f90wrap is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  f90wrap is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with f90wrap. If not, see <http://www.gnu.org/licenses/>.
+# 
+#  If you would like to license the source code under different terms,
+#  please contact James Kermode, james.kermode@gmail.com
+#  f90wrap: F90 to Python interface generator with derived type support
+# 
+#  Copyright James Kermode 2011-2018
+#
+#  This file is part of f90wrap
+#  For the latest version see github.com/jameskermode/f90wrap 
+#
+#  f90wrap is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  f90wrap is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with f90wrap. If not, see <http://www.gnu.org/licenses/>.
+# 
+#  If you would like to license the source code under different terms,
+#  please contact James Kermode, james.kermode@gmail.com
+# updates the copyright information for all .py files
+# usage: call recursive_traversal, with the following parameters
+# parent directory, old copyright text content, new copyright text content
+# adapted from: https://stackoverflow.com/questions/151677/tool-for-adding-license-headers-to-source-files
+
+import os
+
+excludedir = ["./build", "./.git"]
+
+def update_source(filename, oldcopyright, copyright):
+    utfstr = chr(0xef)+chr(0xbb)+chr(0xbf)
+    fdata = file(filename,"r+").read()
+    isUTF = False
+    if (fdata.startswith(utfstr)):
+        isUTF = True
+        fdata = fdata[3:]
+    if (oldcopyright != None):
+        if (fdata.startswith(oldcopyright)):
+            fdata = fdata[len(oldcopyright):]
+    if not (fdata.startswith(copyright)):
+        print "updating "+filename
+        fdata = copyright + fdata
+        if (isUTF):
+            file(filename,"w").write(utfstr+fdata)
+        else:
+            file(filename,"w").write(fdata)
+
+def recursive_traversal(dir,  oldcopyright, copyright):
+    global excludedir
+    fns = os.listdir(dir)
+    print "listing "+dir
+    for fn in fns:
+        fullfn = os.path.join(dir,fn)
+        if (fullfn in excludedir):
+            continue
+        if (os.path.isdir(fullfn)):
+            recursive_traversal(fullfn, oldcopyright, copyright)
+        else:
+            if (fullfn.endswith(".py")):
+                update_source(fullfn, oldcopyright, copyright)
+
+oldcright = file("oldcr.txt","r+").read()
+cright = file("copyrightText.txt","r+").read()
+recursive_traversal(".", oldcright, cright)
+exit()
