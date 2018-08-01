f53397088ffb9f884651c48377c0f4f68a7c9e38
Michael Lange <Michael.Lange@ecmwf.int>
2018-08-01 11:50:37 +0100

    Subroutine: Add test for commented multi-line dummy arguments
    Line continuation is often used for long "Fortran-style" dummy argument
    lists, but seems to break the argument detection in `f90wrap` if
    comments are intersperesed with the multiline argument list.

---
+++ examples/subroutine_args/subroutine_mod.f90
@@ -0,0 +1,39 @@
+module subroutine_mod
+
+    implicit none
+
+contains
+
+    subroutine routine_with_simple_args(a, b, c, d)
+      integer, intent(in) :: a, b
+      integer, intent(out) :: c, d
+
+      c = a + b
+      d = a * b
+
+    end subroutine routine_with_simple_args
+
+    subroutine routine_with_multiline_args(&
+         & a, b, c, d)
+      integer, intent(in) :: a, b
+      integer, intent(out) :: c, d
+
+      c = a + b
+      d = a * b
+
+    end subroutine routine_with_multiline_args
+
+    ! subroutine routine_with_commented_args(&
+    !      ! Some arbitrary comment...
+    !      & a, b, &
+    !      ! ... to highlight Fortran line continuation.
+    !      & c, d)
+    !   integer, intent(in) :: a, b
+    !   integer, intent(out) :: c, d
+
+    !   c = a + b
+    !   d = a * b
+
+    ! end subroutine routine_with_commented_args
+
+end module subroutine_mod
