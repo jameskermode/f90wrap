fd295550e60dab575673bb4f747d7bba9c731a47
James Kermode <J.R.Kermode@warwick.ac.uk>
2021-05-21 14:28:27 +0100

    bump version

--- f90wrap/__init__.py
+++ f90wrap/__init__.py
@@ -47,7 +47,7 @@ wrapping Fortran code with a simple f90 interface layer.
 
 (c) James Kermode 2011-2021 <james.kermode@gmail.com>"""
 
-__version__ = '0.2.3'
+__version__ = '0.2.4'
 
 import sys
 major, minor = sys.version_info[0:2]
