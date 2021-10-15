01665381aaa875de0cf09301488ab70c9ed8fb32
Ananth Sridharan <ananth.sridharan@vtolanalytics.com>
2021-10-15 13:57:23 -0700

    removed math library to simplify example

--- examples/reserved_words/driver.py
+++ examples/reserved_words/driver.py
@@ -8,14 +8,3 @@ type_real = type(sd.test_float)
 print('type of DP   designation is ',type_DP)
 print('type of SP   designation is ',type_SP)
 print('type of real designation is ',type_real)
-
-N=5
-rcond=0.0
-determ=0.0
-atemp = numpy.asfortranarray(numpy.zeros((N,N)))
-for i in range(N):
-    atemp[i,i] = i+1
-
-test.invert(atemp,N,N,rcond,determ)
-
-print(atemp)
--- examples/reserved_words/src/driver.F90
+++ examples/reserved_words/src/driver.F90
@@ -10,11 +10,6 @@ integer :: i
 
 type(outer) :: size_data
 
-do i=1,N
-    atemp(i,i) = real(i)
-end do 
-write(*,*) 'hello',kind(atemp(1,1))
-call invert(atemp,N,N,rcond,determ)
 write(*,*) atemp
 write(*,*) 'from pyinv_mod'
 write(*,*) size_data%size%test_double, kind(size_data%size%test_double)
