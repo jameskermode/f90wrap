03709c44326e4790420a02163f66f5da71c57a37
Christopher Albert <albert@alumni.tugraz.at>
2024-11-14 20:12:07 +0000

    Implementation added

---
+++ examples/issue41_abstract_classes/myclass_impl.f90
@@ -0,0 +1,20 @@
+module myclass_impl
+
+use myclass_base, only: myclass_t
+implicit none
+
+type, extends(myclass_t) :: myclass_impl_t
+contains
+    procedure :: get_value => get_value_impl
+end type myclass_impl_t
+
+contains
+
+subroutine get_value_impl(self, value)
+    class(myclass_impl_t), intent(in) :: self
+    real, intent(out) :: value
+
+    value = 1.0
+end subroutine get_value_impl
+
+end module myclass_impl
