2b186e8cc265285efded3a24df5b64cb7adb17cc
Christopher Albert <albert@alumni.tugraz.at>
2024-11-15 20:20:47 +0000

    Allow allocatable only for types

--- examples/issue227_allocatable/alloc_output.f90
+++ examples/issue227_allocatable/alloc_output.f90
@@ -7,10 +7,37 @@ end type alloc_output_type
 
 contains
 
-function alloc_output_func() result(out)
+! This should be used by the wrapper generator
+function alloc_output_type_func(val) result(out)
+    real, intent(in) :: val
     type(alloc_output_type), allocatable :: out
     allocate(out)
-    out%a = 1.0
-end function alloc_output_func
+    out%a = val
+end function alloc_output_type_func
+
+
+! This should be discarded by the wrapper generator
+function alloc_output_intrinsic_func(val) result(out)
+    real, intent(in) :: val
+    real, allocatable :: out
+    allocate(out)
+    out = val
+end function alloc_output_intrinsic_func
+
+
+! This should be discarded by the wrapper generator
+function alloc_output_array_func(val) result(out)
+    real, intent(in) :: val(:)
+    real, allocatable :: out(:)
+    allocate(out(size(val)))
+    out(:) = val
+end function alloc_output_array_func
+
+
+subroutine noalloc_output_subroutine(val, out)
+    real, intent(in) :: val
+    type(alloc_output_type), intent(inout) :: out
+    out%a = val
+end subroutine noalloc_output_subroutine
 
 end module alloc_output
--- examples/issue227_allocatable/run.py
+++ examples/issue227_allocatable/run.py
@@ -1,2 +1,32 @@
 #!/usr/bin/env python
 import itest
+
+
+def main():
+    test_type_output_is_wrapped()
+    test_intrinsic_output_is_not_wrapped()
+    test_array_output_is_not_wrapped()
+    test_type_output_wrapper()
+
+
+def test_type_output_is_wrapped():
+    assert hasattr(itest.alloc_output, 'alloc_output_type_func')
+
+
+def test_intrinsic_output_is_not_wrapped():
+    assert (not hasattr(itest.alloc_output, 'alloc_output_intrinsic_func'))
+
+
+def test_array_output_is_not_wrapped():
+    assert (not hasattr(itest.alloc_output, 'alloc_output_array_func'))
+
+
+def test_type_output_wrapper():
+    VAL = 10.0
+    TOL = 1e-13
+
+    t = itest.alloc_output.alloc_output_type_func(VAL)
+    assert(abs(t.a - VAL) < TOL)
+
+if __name__ == '__main__':
+    main()
