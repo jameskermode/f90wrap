4191fc21aedc807a7e7f4f322f5d22c1459df1a5
Christopher Albert <albert@alumni.tugraz.at>
2024-11-15 20:51:55 +0000

    Test memory leaks

--- examples/issue227_allocatable/run.py
+++ examples/issue227_allocatable/run.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
+import os
+import gc
+import tracemalloc
+
 import itest
 
 
@@ -7,6 +11,7 @@ def main():
     test_intrinsic_output_is_not_wrapped()
     test_array_output_is_not_wrapped()
     test_type_output_wrapper()
+    test_memory_leak()
 
 
 def test_type_output_is_wrapped():
@@ -21,12 +26,29 @@ def test_array_output_is_not_wrapped():
     assert (not hasattr(itest.alloc_output, 'alloc_output_array_func'))
 
 
-def test_type_output_wrapper():
-    VAL = 10.0
-    TOL = 1e-13
+VAL = 10.0
+TOL = 1e-13
 
+
+def test_type_output_wrapper():
     t = itest.alloc_output.alloc_output_type_func(VAL)
     assert(abs(t.a - VAL) < TOL)
+
+
+def test_memory_leak():
+    gc.collect()
+    t = []
+    tracemalloc.start()
+    start_snapshot = tracemalloc.take_snapshot()
+    for i in range(2048):
+        t.append(itest.alloc_output.alloc_output_type_func(VAL))
+    del t
+    gc.collect()
+    end_snapshot = tracemalloc.take_snapshot()
+    tracemalloc.stop()
+    stats = end_snapshot.compare_to(start_snapshot, 'lineno')
+    assert sum(stat.size_diff for stat in stats) < 1024
+
 
 if __name__ == '__main__':
     main()
