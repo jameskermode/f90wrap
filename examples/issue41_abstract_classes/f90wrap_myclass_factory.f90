521fdc6f0b9ddc7bea88a6983b8275f07b0511f1
Christopher Albert <albert@tugraz.at>
2024-11-23 22:39:04 +0100

    Debugging reference counting

--- examples/issue41_abstract_classes/f90wrap_myclass_factory.f90
+++ examples/issue41_abstract_classes/f90wrap_myclass_factory.f90
@@ -3,17 +3,10 @@
 subroutine f90wrap_myclass_factory__create_myclass(ret_myobject, impl_type)
     use myclass_factory, only: create_myclass
     use myclass_impl, only: myclass_impl_t
+    use myclass_impl_reference_storage, only: myclass_impl_wrapper_t, &
+        myclass_impl_t_ptr_type, push_reference, reference_storage
     implicit none
 
-    type myclass_impl_wrapper_t
-        class(myclass_impl_t), allocatable :: obj
-    end type myclass_impl_wrapper_t
-
-    type myclass_impl_t_ptr_type
-        type(myclass_impl_wrapper_t), pointer :: p => NULL()
-    end type myclass_impl_t_ptr_type
-
-    type(myclass_impl_wrapper_t), target :: ret_myobject_wrapper
     type(myclass_impl_t_ptr_type) :: ret_myobject_ptr
     integer, intent(out), dimension(2) :: ret_myobject
 
@@ -21,11 +14,11 @@ subroutine f90wrap_myclass_factory__create_myclass(ret_myobject, impl_type)
 
     print *, "create_myclass: ", impl_type
 
-    ret_myobject_wrapper%obj = create_myclass(impl_type)
+    call push_reference()
+    reference_storage(size(reference_storage))%obj = create_myclass(impl_type)
 
-    ret_myobject_ptr%p => ret_myobject_wrapper
+    ret_myobject_ptr%p => reference_storage(size(reference_storage))
     ret_myobject = transfer(ret_myobject_ptr, ret_myobject)
-    call f90wrap_myclass_impl__reference_store(ret_myobject, 1)
 end subroutine f90wrap_myclass_factory__create_myclass
 
 ! End of module myclass_factory defined in file myclass_factory.f90
