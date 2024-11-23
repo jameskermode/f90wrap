5cc8e9a7b56050dd8dcc8b23c03fcf90e2c3d3fd
Christopher Albert <albert@tugraz.at>
2024-11-23 19:39:36 +0100

    Add finaliser for testing

--- examples/issue41_abstract_classes/run.py
+++ examples/issue41_abstract_classes/run.py
@@ -5,4 +5,6 @@ REF = 1.0
 TOL = 1.0e-14
 
 obj = itest.myclass_factory.create_myclass("impl")
-assert(abs(obj.get_value()-REF)<TOL)
+output = obj.get_value()
+assert(abs(output-REF)<TOL)
+print(f"OK: {output} == {REF}")
