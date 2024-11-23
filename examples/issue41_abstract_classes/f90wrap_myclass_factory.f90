a53595645bc09cb315d926ff015062945277b6b6
Christopher Albert <albert@tugraz.at>
2024-11-23 21:30:21 +0100

    Temporarily adding manual wrappers

---
+++ examples/issue41_abstract_classes/f90wrap_myclass_factory.f90
@@ -0,0 +1,29 @@
+! Module myclass_factory defined in file myclass_factory.f90
+
+subroutine f90wrap_myclass_factory__create_myclass(ret_myobject, impl_type)
+    use myclass_factory, only: create_myclass
+    use myclass_impl, only: myclass_impl_t
+    implicit none
+
+    type myclass_impl_wrapper_t
+        class(myclass_impl_t), allocatable :: obj
+    end type myclass_impl_wrapper_t
+
+    type myclass_impl_t_ptr_type
+        type(myclass_impl_wrapper_t), pointer :: p => NULL()
+    end type myclass_impl_t_ptr_type
+
+    type(myclass_impl_wrapper_t), target :: ret_myobject_wrapper
+    type(myclass_impl_t_ptr_type) :: ret_myobject_ptr
+    integer, intent(out), dimension(2) :: ret_myobject
+
+    character*(*), intent(in) :: impl_type
+
+    print *, "create_myclass: ", impl_type
+
+    ret_myobject_wrapper%obj = create_myclass(impl_type)
+    ret_myobject_ptr%p => ret_myobject_wrapper
+    ret_myobject = transfer(ret_myobject_ptr, ret_myobject)
+end subroutine f90wrap_myclass_factory__create_myclass
+
+! End of module myclass_factory defined in file myclass_factory.f90
