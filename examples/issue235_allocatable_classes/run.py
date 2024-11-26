f5eb87a6a65c41aeff609bbd2750482b97b89003
Christopher Albert <albert@tugraz.at>
2024-11-26 20:46:58 +0100

    Fix run.py for issue235

--- examples/issue235_allocatable_classes/run.py
+++ examples/issue235_allocatable_classes/run.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python
 import itest
 
-REF = 1.0
+REF = 3.1415
 TOL = 1.0e-14
 
-obj = itest.myclass_factory.create_myclass("impl")
+obj = itest.myclass_factory.myclass_create(REF)
 
-output = obj.get_value()
+output = obj.get_val()
 assert(abs(output-REF)<TOL)
 print(f"OK: {output} == {REF}")
-
-# obj2 = itest.myclass_factory.create_myclass("impl")
-# output2 = obj2.get_value()
-# assert(abs(output-REF)<TOL)
-# print(f"OK: {output2} == {REF}")
