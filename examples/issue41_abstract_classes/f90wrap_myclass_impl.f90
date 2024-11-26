29b207a4b904ead4b5a16967220f5c71ea0a3084
Christopher Albert <albert@tugraz.at>
2024-11-26 17:31:00 +0100

    Working for types

--- examples/issue41_abstract_classes/f90wrap_myclass_impl.f90
+++ examples/issue41_abstract_classes/f90wrap_myclass_impl.f90
@@ -1,46 +1,39 @@
+! Module myclass_impl defined in file myclass_impl.f90
+
 subroutine f90wrap_myclass_impl__myclass_impl_finalise__binding__mycla4a60(self)
     use myclass_impl, only: myclass_impl_t, myclass_impl_finalise
     implicit none
 
-    type myclass_impl_wrapper_t
-        type(myclass_impl_t), allocatable :: obj
-    end type myclass_impl_wrapper_t
     type myclass_impl_t_ptr_type
-        type(myclass_impl_wrapper_t), pointer :: p => NULL()
+        type(myclass_impl_t), pointer :: p => NULL()
     end type myclass_impl_t_ptr_type
     type(myclass_impl_t_ptr_type) :: self_ptr
     integer, intent(in), dimension(2) :: self
     self_ptr = transfer(self, self_ptr)
-    !call myclass_impl_finalise(self=self_ptr%p)
-    !deallocate(self_ptr%p)
+    call myclass_impl_finalise(self=self_ptr%p)
+    deallocate(self_ptr%p)
 end subroutine f90wrap_myclass_impl__myclass_impl_finalise__binding__mycla4a60
 
 subroutine f90wrap_myclass_impl__myclass_impl_finalise(self)
     use myclass_impl, only: myclass_impl_t, myclass_impl_finalise
     implicit none
 
-    type myclass_impl_wrapper_t
-        type(myclass_impl_t), allocatable :: obj
-    end type myclass_impl_wrapper_t
     type myclass_impl_t_ptr_type
-        type(myclass_impl_wrapper_t), pointer :: p => NULL()
+        type(myclass_impl_t), pointer :: p => NULL()
     end type myclass_impl_t_ptr_type
     type(myclass_impl_t_ptr_type) :: self_ptr
     integer, intent(in), dimension(2) :: self
     self_ptr = transfer(self, self_ptr)
-    print *, self
-    deallocate(self_ptr%p%obj)
+    !call myclass_impl_finalise(self=self_ptr%p)
+    deallocate(self_ptr%p)
 end subroutine f90wrap_myclass_impl__myclass_impl_finalise
 
 subroutine f90wrap_myclass_impl__myclass_impl_t_initialise(this)
     use myclass_impl, only: myclass_impl_t
     implicit none
 
-    type myclass_impl_wrapper_t
-        type(myclass_impl_t), allocatable :: obj
-    end type myclass_impl_wrapper_t
     type myclass_impl_t_ptr_type
-        type(myclass_impl_wrapper_t), pointer :: p => NULL()
+        type(myclass_impl_t), pointer :: p => NULL()
     end type myclass_impl_t_ptr_type
     type(myclass_impl_t_ptr_type) :: this_ptr
     integer, intent(out), dimension(2) :: this
@@ -52,34 +45,28 @@ subroutine f90wrap_myclass_impl__get_value__binding__myclass_impl_t(self, value)
     use myclass_impl, only: myclass_impl_t
     implicit none
 
-    type myclass_impl_wrapper_t
-        type(myclass_impl_t), allocatable :: obj
-    end type myclass_impl_wrapper_t
     type myclass_impl_t_ptr_type
-        type(myclass_impl_wrapper_t), pointer :: p => NULL()
+        type(myclass_impl_t), pointer :: p => NULL()
     end type myclass_impl_t_ptr_type
     type(myclass_impl_t_ptr_type) :: self_ptr
     integer, intent(in), dimension(2) :: self
     real, intent(out) :: value
     self_ptr = transfer(self, self_ptr)
-    call self_ptr%p%obj%get_value(value=value)
+    call self_ptr%p%get_value(value=value)
 end subroutine f90wrap_myclass_impl__get_value__binding__myclass_impl_t
 
 subroutine f90wrap_myclass_impl__get_value_impl(self, value)
-    use myclass_impl, only: myclass_impl_t, get_value_impl
+    use myclass_impl, only: get_value_impl, myclass_impl_t
     implicit none
 
-    type myclass_impl_wrapper_t
-        type(myclass_impl_t), allocatable :: obj
-    end type myclass_impl_wrapper_t
     type myclass_impl_t_ptr_type
-        type(myclass_impl_wrapper_t), pointer :: p => NULL()
+        type(myclass_impl_t), pointer :: p => NULL()
     end type myclass_impl_t_ptr_type
     type(myclass_impl_t_ptr_type) :: self_ptr
     integer, intent(in), dimension(2) :: self
     real, intent(out) :: value
     self_ptr = transfer(self, self_ptr)
-    call get_value_impl(self=self_ptr%p%obj, value=value)
+    call get_value_impl(self=self_ptr%p, value=value)
 end subroutine f90wrap_myclass_impl__get_value_impl
 
 ! End of module myclass_impl defined in file myclass_impl.f90
