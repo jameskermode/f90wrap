ab1a478a0177d29b9bd41d39c43c96b1889cf093
Christopher Albert <albert@tugraz.at>
2024-11-26 18:11:33 +0100

    Working for class

--- examples/issue41_abstract_classes/f90wrap_myclass_factory.f90
+++ examples/issue41_abstract_classes/f90wrap_myclass_factory.f90
@@ -5,14 +5,17 @@ subroutine f90wrap_myclass_factory__create_myclass(ret_myobject, impl_type)
     use myclass_factory, only: create_myclass
     implicit none
 
+    type :: myclass_wrapper_t
+        class(myclass_impl_t), allocatable :: obj
+    end type myclass_wrapper_t
     type myclass_impl_t_ptr_type
-        type(myclass_impl_t), pointer :: p => NULL()
+        type(myclass_wrapper_t), pointer :: p => NULL()
     end type myclass_impl_t_ptr_type
     type(myclass_impl_t_ptr_type) :: ret_myobject_ptr
     integer, intent(out), dimension(2) :: ret_myobject
     character*(*), intent(in) :: impl_type
     allocate(ret_myobject_ptr%p)
-    ret_myobject_ptr%p = create_myclass(impl_type=impl_type)
+    ret_myobject_ptr%p%obj = create_myclass(impl_type=impl_type)
     ret_myobject = transfer(ret_myobject_ptr, ret_myobject)
 end subroutine f90wrap_myclass_factory__create_myclass
 
