148d0b9ad67dcd17c4cdb04e09a25696d3e7b9de
Christopher Albert <albert@tugraz.at>
2024-11-26 22:23:46 +0100

    Get and set still failing

--- examples/issue235_allocatable_classes/run.py
+++ examples/issue235_allocatable_classes/run.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python
 import pytest
-from itest.myclass import get_create_count, get_destroy_count
+from itest.myclass import (
+    get_create_count,
+    get_destroy_count,
+    set_create_count,
+    set_destroy_count,
+)
 from itest.myclass_factory import myclass_create
 
 REF = 3.1415
 TOL = 1.0e-6
 
+
 def test_create_destroy_object():
-    assert get_create_count() == 0
-    assert get_destroy_count() == 0
+    set_create_count(0)
+    set_destroy_count(0)
 
     obj = myclass_create(REF)
 
@@ -22,5 +28,30 @@ def test_create_destroy_object():
     assert get_create_count() == 1
     assert get_destroy_count() == 1
 
-if __name__ == '__main__':
+
+def test_getter_setter():
+    obj = myclass_create(REF)
+
+    assert abs(obj.get_val() - REF) < TOL
+
+    obj.set_val(2.0 * REF)
+
+    assert abs(obj.get_val() - 2.0 * REF) < TOL
+
+    del obj
+
+
+def test_get_set_direct():
+    obj = myclass_create(REF)
+
+    assert abs(obj.val - REF) < TOL
+
+    obj.val = 2.0 * REF
+
+    assert abs(obj.val - 2.0 * REF) < TOL
+
+    del obj
+
+
+if __name__ == "__main__":
     pytest.main([__file__])
