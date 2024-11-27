57e4b9fcec6959204f4f21a5e69d93cdb7e4e4ea
Christopher Albert <albert@tugraz.at>
2024-11-27 23:44:42 +0100

    Working template for abstract class

--- examples/issue41_abstract_classes/myclass_factory.f90
+++ examples/issue41_abstract_classes/myclass_factory.f90
@@ -1,12 +1,13 @@
 module myclass_factory
 
+use myclass_base, only: myclass_t
 use myclass_impl, only: myclass_impl_t
 implicit none
 
 contains
 
 function create_myclass(impl_type) result(myobject)
-    type(myclass_impl_t), allocatable :: myobject
+    class(myclass_t), allocatable :: myobject
 
     character(*), intent(in) :: impl_type
 
