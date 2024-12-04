8cb4a011a0fd8b38c738ae60599f38927b563149
Christopher Albert <albert@alumni.tugraz.at>
2024-12-04 11:04:18 +0000

    Rename interface to distinguish

--- examples/issue41_abstract_classes/myclass_base.f90
+++ examples/issue41_abstract_classes/myclass_base.f90
@@ -3,15 +3,15 @@ implicit none
 
 type, abstract :: myclass_t
 contains
-    procedure(get_value), deferred :: get_value
+    procedure(get_value_i), deferred :: get_value
 end type myclass_t
 
 abstract interface
-    subroutine get_value(self, value)
+    subroutine get_value_i(self, value)
         import myclass_t
         class(myclass_t), intent(in) :: self
         real, intent(out) :: value
-    end subroutine get_value
+    end subroutine get_value_i
 end interface
 
 end module myclass_base
