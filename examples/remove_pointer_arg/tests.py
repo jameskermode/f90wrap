6861a4b23fa4118545a65510f9e624464b5e7951
Christopher Albert <albert@alumni.tugraz.at>
2025-01-05 17:46:21 +0100

    Fix #242 Meson tests

--- examples/remove_pointer_arg/tests.py
+++ examples/remove_pointer_arg/tests.py
@@ -4,14 +4,14 @@ from pywrapper import m_test
 
 class TestReturnArray(unittest.TestCase):
 
-    def not_ignored(self):
+    def test_not_ignored(self):
         _ = m_test.not_to_be_ignored()
 
-    def ignored_1(self):
+    def test_ignored_1(self):
         with self.assertRaises(AttributeError):
             _ = m_test.to_be_ignored_1()
 
-    def ignored_2(self):
+    def test_ignored_2(self):
         with self.assertRaises(AttributeError):
             _ = m_test.to_be_ignored_2()
 
